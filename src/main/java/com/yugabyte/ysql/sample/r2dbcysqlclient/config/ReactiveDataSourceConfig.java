3355f21978618094b5fc294fc3cb40a5af1c01c1
Yogendra Rampuria - Yogi <yogendrarampuria@gmail.com>
2022-08-12 15:56:06 +0800

    Upgrade java, spring and r2dbc version. Added lombok, devtools, and actuator Updated code to use new DatabaseClient class.

--- src/main/java/com/yugabyte/ysql/sample/r2dbcysqlclient/config/ReactiveDataSourceConfig.java
+++ src/main/java/com/yugabyte/ysql/sample/r2dbcysqlclient/config/ReactiveDataSourceConfig.java
@@ -1,40 +1,21 @@
 package com.yugabyte.ysql.sample.r2dbcysqlclient.config;
 
-import org.springframework.beans.factory.annotation.Value;
+import com.yugabyte.ysql.sample.r2dbcysqlclient.repository.ProductRepository;
+import io.r2dbc.spi.ConnectionFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.r2dbc.config.AbstractR2dbcConfiguration;
 import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
-
-import com.yugabyte.ysql.sample.r2dbcysqlclient.repository.ProductRepository;
-
-import io.r2dbc.postgresql.PostgresqlConnectionConfiguration;
-import io.r2dbc.postgresql.PostgresqlConnectionFactory;
+import org.springframework.r2dbc.core.DatabaseClient;
 
 @Configuration
 @EnableR2dbcRepositories(basePackageClasses = ProductRepository.class)
-public class ReactiveDataSourceConfig extends AbstractR2dbcConfiguration {
-
-	@Value("${yugabyte.ysql.datasource.host:localhost}")
-	private String host;
-	@Value("${yugabyte.ysql.datasource.port:5433}")
-	private int port;
-	@Value("${yugabyte.ysql.datasource.database:yugabyte}")
-	private String database;
-	@Value("${datasource.username:yugabyte}")
-	private String username;
-	
-	@Bean(name = "r2dbcConnection")
-	@Override
-	public PostgresqlConnectionFactory connectionFactory() {
-		
-		return new PostgresqlConnectionFactory(PostgresqlConnectionConfiguration
-				.builder()
-				.host(host)
-				.port(port)
-				.username(username)
-				.database(database)
-				.build());	
-	}
-
+public class ReactiveDataSourceConfig  {
+  @Bean
+  DatabaseClient databaseClient(ConnectionFactory connectionFactory) {
+    return DatabaseClient.builder()
+      .connectionFactory(connectionFactory)
+      //.bindMarkers(() -> BindMarkersFactory.named(":", "", 20).create())
+      .namedParameters(true)
+      .build();
+  }
 }
