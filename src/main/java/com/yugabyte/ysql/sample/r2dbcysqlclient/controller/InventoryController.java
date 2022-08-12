941c43d462f76c04c451e02beaa5271f921c996b
Yogendra Rampuria - Yogi <yogendrarampuria@gmail.com>
2022-08-12 15:57:02 +0800

    Cleaned up code

--- src/main/java/com/yugabyte/ysql/sample/r2dbcysqlclient/controller/InventoryController.java
+++ src/main/java/com/yugabyte/ysql/sample/r2dbcysqlclient/controller/InventoryController.java
@@ -1,36 +1,35 @@
 package com.yugabyte.ysql.sample.r2dbcysqlclient.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import com.yugabyte.ysql.sample.r2dbcysqlclient.domain.Inventory;
+import com.yugabyte.ysql.sample.r2dbcysqlclient.repository.InventoryRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.yugabyte.ysql.sample.r2dbcysqlclient.domain.Inventory;
-import com.yugabyte.ysql.sample.r2dbcysqlclient.domain.Product;
-import com.yugabyte.ysql.sample.r2dbcysqlclient.repository.InventoryRepository;
-
 import reactor.core.publisher.Mono;
 
 @RestController
 public class InventoryController {
-	
-	@Autowired
-	private InventoryRepository inventoryRepository;
 
-	@PostMapping("/inventory/add")
-	public Mono<Inventory> addInventoryDetailsForProduct(@RequestBody Inventory inventory) {
+	private final InventoryRepository inventoryRepository;
+
+  public InventoryController(InventoryRepository inventoryRepository) {
+    this.inventoryRepository = inventoryRepository;
+  }
+
+  @PostMapping("/inventory/add")
+	public Mono<Inventory> add(@RequestBody Inventory inventory) {
 		return inventoryRepository.save(inventory);
 	}
 
-	@GetMapping("/inventory/get/{productId}")
-	public Mono<Inventory> getInventoryById(@PathVariable Integer inventoryId, @RequestBody Product productRequest) {
+	@GetMapping("/inventory/get/{inventoryId}")
+	public Mono<Inventory> get(@PathVariable Integer inventoryId) {
 		return inventoryRepository.findById(inventoryId);
 	}
 
-	@GetMapping("/inventory/delete/{productId}")
-	public Mono<Void> deleteInventoryById(@PathVariable Integer inventoryId) {
+	@GetMapping("/inventory/delete/{inventoryId}")
+	public Mono<Void> delete(@PathVariable Integer inventoryId) {
 		return inventoryRepository.deleteById(inventoryId);
 	}
 
